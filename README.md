23ad14e34ab23ba1d3e3ac91a05def3eede7ccaa
Eric Zhang <ekzhang1@gmail.com>
2022-05-06 17:24:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cadre
+
+High-performance, minimal remote configuration service that's extremely simple to set up and use.
---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! Cadre is a simple, self-hosted, high-performance, and strongly consistent
+//! remote configuration service.
+
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
