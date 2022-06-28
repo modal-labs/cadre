1e3d57b325aa17db6e9d089a9bd15bc86087ecd7
Eric Zhang <ekzhang1@gmail.com>
2022-06-28 14:56:30 -0400

    Add unsafe + missing docs lint, refactor templates

--- tests/template_test.rs
+++ tests/template_test.rs
@@ -9,7 +9,7 @@ async fn parse_test() -> Result<()> {
     let json = json!({"name": "test"});
     let observed = json.clone();
     let expected = json.clone();
-    let mut template = Template::new(&config, observed).await?;
+    let template = Template::new(&config, observed).await?;
 
     assert_eq!(template.parse().await?, expected);
     Ok(())
@@ -19,7 +19,7 @@ async fn parse_test() -> Result<()> {
 async fn parse_test_fail() -> Result<()> {
     let config = default_aws_config().await?;
     let observed = Value::from("[]");
-    let mut template = Template::new(&config, observed).await?;
+    let template = Template::new(&config, observed).await?;
 
     let value = template.parse().await;
     assert!(value.is_err());
