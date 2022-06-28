cf3202693bf0b7a88f32c8cec972106251ba253b
Eric Zhang <ekzhang1@gmail.com>
2022-06-28 17:22:50 -0400

    Add a cache to the secrets client

--- src/secrets.rs
+++ src/secrets.rs
@@ -1,14 +1,19 @@
 //! Interface for secret retrieval from the AWS Secret Manager service.;
 
+use std::sync::Arc;
+
 use anyhow::{Context, Result};
 use aws_sdk_secretsmanager::Client;
 use aws_types::sdk_config::SdkConfig;
+use cached::{Cached, TimedCache};
+use parking_lot::Mutex;
 use serde_json::Value;
 
 /// Objects that manages the retrieval of secrets.
 #[derive(Clone, Debug)]
 pub struct Secrets {
     client: Option<Client>,
+    cache: Arc<Mutex<TimedCache<String, Value>>>,
 }
 
 impl Secrets {
@@ -17,26 +22,37 @@ impl Secrets {
         let client = Client::new(aws_config);
         Self {
             client: Some(client),
+            cache: Arc::new(Mutex::new(TimedCache::with_lifespan(60))),
         }
     }
 
     /// Creates a new instance with no backing secrets manager.
     pub fn new_test() -> Self {
-        Self { client: None }
+        Self {
+            client: None,
+            cache: Arc::new(Mutex::new(TimedCache::with_lifespan(60))),
+        }
     }
 
     /// Fetches a secret from the AWS Secret Manager.
     pub async fn get(&self, name: &str) -> Result<Value> {
+        let name = name.to_string();
+        if let Some(value) = self.cache.lock().cache_get(&name) {
+            return Ok(value.clone());
+        }
+
         let resp = self
             .client
             .as_ref()
             .context("AWS secrets client is missing")?
             .get_secret_value()
-            .secret_id(name)
+            .secret_id(&name)
             .send()
             .await?;
 
         let secret = resp.secret_string().context("missing secret string")?;
-        Ok(serde_json::from_str(secret)?)
+        let value: Value = serde_json::from_str(secret)?;
+        self.cache.lock().cache_set(name, value.clone());
+        Ok(value)
     }
 }
