88eb592e6baac8a9560c631e67a033f946bad976
Eric Zhang <ekzhang1@gmail.com>
2022-06-28 15:11:37 -0400

    Get rid of the NOT_ACCEPTABLE stuff

--- src/server.rs
+++ src/server.rs
@@ -4,8 +4,6 @@
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
 
-use std::collections::HashMap;
-
 use anyhow::Result;
 use axum::extract::{Extension, Path};
 use axum::routing::{get, put};
@@ -21,45 +19,37 @@ pub async fn server(bucket: &str, default_template: Option<&str>) -> Result<Rout
 
     Ok(Router::new()
         .route("/", get(|| async { Html(include_str!("index.html")) }))
-        .route("/w/:environment", put(put_handler))
-        .route("/t/:environment", get(get_template_handler))
+        .route("/w/:env", put(put_handler))
+        .route("/t/:env", get(get_template_handler))
         .route("/c", get(get_all_configs_handler))
-        .route("/c/:environment", get(get_config_handler))
+        .route("/c/:env", get(get_config_handler))
         .route("/ping", get(|| async { "cadre ok" }))
         .layer(Extension(storage)))
 }
 
 async fn get_template_handler(
     Extension(storage): Extension<Storage>,
-    Path(params): Path<HashMap<String, String>>,
+    Path(env): Path<String>,
 ) -> Result<Json<Value>, StatusCode> {
-    if let Some(environment) = params.get("environment") {
-        match storage.read_template(environment).await {
-            Ok(value) => Ok(Json(value)),
-            Err(err) => {
-                warn!(%environment, ?err, "problem getting template");
-                Err(StatusCode::NOT_FOUND)
-            }
+    match storage.read_template(&env).await {
+        Ok(value) => Ok(Json(value)),
+        Err(err) => {
+            warn!(%env, ?err, "problem getting template");
+            Err(StatusCode::NOT_FOUND)
         }
-    } else {
-        Err(StatusCode::NOT_ACCEPTABLE)
     }
 }
 
 async fn get_config_handler(
     Extension(storage): Extension<Storage>,
-    Path(params): Path<HashMap<String, String>>,
+    Path(env): Path<String>,
 ) -> Result<Json<Value>, StatusCode> {
-    if let Some(environment) = params.get("environment") {
-        match storage.read_config(environment).await {
-            Ok(value) => Ok(Json(value)),
-            Err(err) => {
-                warn!(%environment, ?err, "problem reading config");
-                Err(StatusCode::NOT_FOUND)
-            }
+    match storage.read_config(&env).await {
+        Ok(value) => Ok(Json(value)),
+        Err(err) => {
+            warn!(%env, ?err, "problem reading config");
+            Err(StatusCode::NOT_FOUND)
         }
-    } else {
-        Err(StatusCode::NOT_ACCEPTABLE)
     }
 }
 
@@ -77,18 +67,14 @@ async fn get_all_configs_handler(
 
 async fn put_handler(
     Extension(storage): Extension<Storage>,
-    Path(params): Path<HashMap<String, String>>,
+    Path(env): Path<String>,
     body: Json<Value>,
-) -> Result<StatusCode, StatusCode> {
-    if let Some(environment) = params.get("environment") {
-        match storage.write(environment, &body).await {
-            Ok(()) => Ok(StatusCode::OK),
-            Err(err) => {
-                error!(?err, "could not put config");
-                Err(StatusCode::INTERNAL_SERVER_ERROR)
-            }
+) -> Result<(), StatusCode> {
+    match storage.write(&env, &body).await {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            error!(?err, "could not put config");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
         }
-    } else {
-        Err(StatusCode::NOT_ACCEPTABLE)
     }
 }
