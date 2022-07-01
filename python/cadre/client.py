d8ea6c614d431d17900d5b3eb1ba5afc3c297718
Luis Capelo <luis@modal.com>
2022-07-01 15:37:48 +0000

    Writes Documentation

--- python/cadre/client.py
+++ python/cadre/client.py
@@ -48,19 +48,74 @@ class Client:
         return await self._send(request)
     
     async def ping(self) -> str:
-        """Verifies that Cadre is running by querying its healthcheck endpoint."""
+        """Verifies that Cadre is running by querying its healthcheck endpoint.
+        
+        Returns
+        -------
+        str
+            Original healthcheck message.
+        """
         request = httpx.Request("GET", self._build_uri("ping"))
         return await self._send(request, parse_json=False)
 
     async def get_template(self, env:str) -> dict[str, Any]:
+        """Gets the corresponding JSON template to an environment. This is
+        the original template without the retrieval of secrets.
+        
+        Parameters
+        ----------
+        env: str
+            Environment corresponding to a JSON template. For example: "prod".
+        
+        Returns
+        -------
+        dict[str, Any]
+            Dict representing original JSON template. This will contain
+            unparsed templating marks (e.g. {*"test": "aws:prod/Secret"}).
+        """
         return await self._get(f"t/{env}")
     
     async def load_config(self, env:str) -> dict[str, Any]:
+        """Gets the parsed JSON template populated with secrets from
+        a secrets store (e.g. AWS Secrets Manager).
+        
+        Parameters
+        ----------
+        env: str
+            Environment corresponding to a JSON template. For example: "prod".
+        
+        Returns
+        -------
+        dict[str, Any]
+            Dict with parsed JSON template. Template keys and values are
+            replaced with values from a secret store (e.g. AWS Secrets Manager).
+            Example:
+
+                - {"*test": "aws:prod/Secret"} is replaced with
+                - {"test": "my-secret-value"}, where "my-secret-value" is
+                  retrieved from AWS Secrets Manager
+        """
         return await self._get(f"c/{env}")
     
-    async def list_configs(self) -> dict[str, Any]:
+    async def list_configs(self) -> list[str]:
+        """Lists available configs.
+        
+        Returns
+        -------
+        list[str]
+            List of available configs.
+        """
         return await self._get(f"c")
     
-    async def write_template(self, env:str, json:dict[str, Any]) -> dict[str, Any]:
+    async def write_template(self, env:str, json:dict[str, Any]) -> None:
+        """Writes template JSON config to Cadre.
+
+        Parameters
+        ----------
+        env: str
+            Environment identifier. Example: "dev"
+        json: dict[str, Any]
+            Dict to be converted to JSON. All values must be JSON-serializable.
+        """
         request = httpx.Request("PUT", self._build_uri(f"t/{env}"), json=json)
-        return await self._send(request, parse_json=False)
+        await self._send(request, parse_json=False)
