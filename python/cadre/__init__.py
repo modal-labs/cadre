debd5bcb7bc43c59014bf550bc59f13e14e2dda1
Luis Capelo <luis@modal.com>
2022-07-01 15:53:15 +0000

    Adds setup.py

--- python/cadre/__init__.py
+++ python/cadre/__init__.py
@@ -1,4 +1,5 @@
 from cadre.client import Client, CadreException
 
+__name__ = "cadre"
 __version__ = "0.4.0"
 __all__ = ["Client", "CadreException"]
---
+++ python/setup.py
@@ -0,0 +1,4 @@
+from cadre import __name__, __version__
+from setuptools import setup
+
+setup(name=__name__, version=__version__)
